69ac2dc7045ca5ad300aa07ba79b936e87be5558
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-21 23:44:04 -0700

    starting to punch in first components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Pet from './Pet';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello bla bla bla another bla</h1>
+        <Pet name='Luna' animal='Dog' breed='Havanese' />
+        <Pet name='Pepper' animal='Bird' breed='Cockatiel' />
+        <Pet name='Waffles' animal='Cat' breed='Gray Tabby' />
       </div>
     );
   }
