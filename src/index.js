521e24b21ca164b6d7d394156e6eb9f933b583f5
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-21 22:58:39 -0700

    alright, basic babel / webpack dev environment and compilation works. Two important takeaways: in webpack.config.js, we need the html-webpack-plugin install from npm. second, window and document dont exist when not attached to html or loaded in a browser, so you need to wrap ReactDOM.render() in if (typeof window !== 'undefined') to get the app the render and work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('works');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+if (typeof window !== 'undefined') {
+  const app = document.querySelector('#app');
+  ReactDOM.render(<App />, app);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [path.resolve(__dirname, './src/index.js')],
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './build'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
   },
-  mode: 'development',
-  resolve: {
-    modules: [path.resolve('node_modules'), 'node_modules'],
-  },
-
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.(png|svg|jpg|gif|pdf)$/,
-        use: ['file-loader'],
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
