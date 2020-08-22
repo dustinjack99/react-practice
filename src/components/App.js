8835496bea0891208ed104a70c3a72c0c2bca04e
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-22 00:48:58 -0700

    added searchparams component with first hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello bla bla bla another bla</h1>
-        <Pet name='Luna' animal='Dog' breed='Havanese' />
-        <Pet name='Pepper' animal='Bird' breed='Cockatiel' />
-        <Pet name='Waffles' animal='Cat' breed='Gray Tabby' />
+        <h1 id='something-important'>Adopt Me!</h1>
+        <SearchParams />
       </div>
     );
   }
