f515c71c645ffa34f9c152a5efe9bb6d9a719d9b
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-22 12:46:25 -0700

    got details page to render with reach router - MUST replace a tags with Link tags, and remember to import Link component. It does not hand down automatically

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import SearchParams from './SearchParams';
+import Details from './Details';
+import { Router, Link } from '@reach/router';
 
 class App extends Component {
   render() {
     return (
       <React.StrictMode>
         <div>
-          <h1 id='something-important'>Adopt Me!</h1>
-          <SearchParams />
+          <header>
+            <Link to='/'>Adopt Me!</Link>
+          </header>
+
+          <Router>
+            <SearchParams path='/' />
+            <Details path='/details/:id' />
+          </Router>
         </div>
       </React.StrictMode>
     );
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,35 +1,44 @@
-import React, { Component } from 'react'
-import pet from '@frontendmasters/pet'
+import React, { Component } from 'react';
+import pet from '@frontendmasters/pet';
 
 class Details extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
 
-        this.state = {
-            loading: true
-        }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
     }
 
-    componentDidMount() {
-        pet.animal(this.props.id).then(({animal}) => {
-            this.setState({
-                name: animal.name,
-                animal: animal.type,
-                location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
-                description: animal.description,
-                media: animal.photos,
-                breed: animal.breeds.primary,
-                loading: false
-            })
-        })
-    }
+    const { animal, breed, location, description, name } = this.state;
 
-    constructor() {
-        super()
-    }
-
-    render()
+    return (
+      <div className='details'>
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`} </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default Details
+export default Details;
